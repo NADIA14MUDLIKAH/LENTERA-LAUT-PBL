edb4de4787a839205243cdcd5f6c547b0e317ddc
unknown <tsaraigandhi@gmail.com>
2026-06-07 01:12:37 +0700

    Menambahkan service fetch lokasi

--- frontend/src/app/services/api.ts
+++ frontend/src/app/services/api.ts
@@ -22,3 +22,28 @@ export async function fetchPrediction(locationName: string) {
     throw error;
   }
 }
+
+// Tambahkan di dalam src/app/services/api.ts
+
+export async function fetchLocations() {
+  const url = `http://127.0.0.1:8000/locations`;
+  
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (!response.ok) {
+      throw new Error(`Gagal mengambil data lokasi. Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data; // Mengembalikan array of objects lokasi
+  } catch (error) {
+    console.error("Error pada API Locations:", error);
+    return []; // Kembalikan array kosong jika gagal agar map tidak crash
+  }
+}
